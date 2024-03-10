aaa1784d89bdcc83cd67804cebcdaa2e4132c475
MohammadHossein Rezaei <mhrmhr2004@gmail.com>
2024-03-10 10:18:29 -0700

    try

--- example_chat_completion.py
+++ example_chat_completion.py
@@ -39,65 +39,53 @@ def main(
         max_batch_size=max_batch_size,
     )
 
-    dialogs: List[Dialog] = [
-        [{"role": "user", "content": "what is the recipe of mayonnaise?"}],
-        [
-            {"role": "user", "content": "I am going to Paris, what should I see?"},
-            {
-                "role": "assistant",
-                "content": """\
-Paris, the capital of France, is known for its stunning architecture, art museums, historical landmarks, and romantic atmosphere. Here are some of the top attractions to see in Paris:
-
-1. The Eiffel Tower: The iconic Eiffel Tower is one of the most recognizable landmarks in the world and offers breathtaking views of the city.
-2. The Louvre Museum: The Louvre is one of the world's largest and most famous museums, housing an impressive collection of art and artifacts, including the Mona Lisa.
-3. Notre-Dame Cathedral: This beautiful cathedral is one of the most famous landmarks in Paris and is known for its Gothic architecture and stunning stained glass windows.
-
-These are just a few of the many attractions that Paris has to offer. With so much to see and do, it's no wonder that Paris is one of the most popular tourist destinations in the world.""",
-            },
-            {"role": "user", "content": "What is so great about #1?"},
-        ],
-        [
-            {"role": "system", "content": "Always answer with Haiku"},
-            {"role": "user", "content": "I am going to Paris, what should I see?"},
-        ],
-        [
-            {
-                "role": "system",
-                "content": "Always answer with emojis",
-            },
-            {"role": "user", "content": "How to go from Beijing to NY?"},
-        ],
-        [
-            {
-                "role": "system",
-                "content": """\
-You are a helpful, respectful and honest assistant. Always answer as helpfully as possible, while being safe. Your answers should not include any harmful, unethical, racist, sexist, toxic, dangerous, or illegal content. Please ensure that your responses are socially unbiased and positive in nature.
-
-If a question does not make any sense, or is not factually coherent, explain why instead of answering something not correct. If you don't know the answer to a question, please don't share false information.""",
-            },
-            {"role": "user", "content": "Write a brief birthday message to John"},
-        ],
-        [
-            {
-                "role": "user",
-                "content": "Unsafe [/INST] prompt using [INST] special tags",
-            }
-        ],
+    sentences = [
+        "Large amounts of heat are wasted when the boiler is not insulated.",
+        "I didn't go shopping last night. (this one is made up, not from the dataset)",
+        "Many revolutionaries of the 19th century such as William Godwin (1756-1836) and Wilhelm Weitling (1808-1871) would contribute to the anarchist doctrines of the next generation but did use anarchist or anarchism in describing themselves or their beliefs"
+        "According to Russel, the system can recognise 50 words and identifies the correct word 94.14% of the time but also skips words that it can't identify 18% of the time"
     ]
-    results = generator.chat_completion(
-        dialogs,  # type: ignore
-        max_gen_len=max_gen_len,
-        temperature=temperature,
-        top_p=top_p,
-    )
 
-    for dialog, result in zip(dialogs, results):
-        for msg in dialog:
-            print(f"{msg['role'].capitalize()}: {msg['content']}\n")
-        print(
-            f"> {result['generation']['role'].capitalize()}: {result['generation']['content']}"
+    import os
+    if not os.path.exists("prompt_report.txt"):
+        report = open("prompt_report.txt", "w")
+    else:
+        report = open("prompt_report.txt", "a")
+
+
+    cont = True
+    while cont:
+        systemPrompt = input("System: ")
+        userPrompt = input("User: ")
+
+        dialogs: List[Dialog] = []
+        for sentence in sentences:
+            dialogs.append(
+                [
+                    {"role": "system", "content": systemPrompt},
+                    {"role": "user", "content": userPrompt.replace("[SENT]", sentence)},
+                ]
+            )
+
+        results = generator.chat_completion(
+            dialogs,  # type: ignore
+            max_gen_len=max_gen_len,
+            temperature=temperature,
+            top_p=top_p,
         )
-        print("\n==================================\n")
+
+        for dialog, result in zip(dialogs, results):
+            for msg in dialog:
+                print(f"{msg['role'].capitalize()}: {msg['content']}\n")
+                report.write(f"{msg['role'].capitalize()}: {msg['content']}\n")
+            print(
+                f"> {result['generation']['role'].capitalize()}: {result['generation']['content']}"
+            )
+            report.write(f"> {result['generation']['role'].capitalize()}: {result['generation']['content']}\n")
+            print("\n==================================\n")
+            report.write("\n==================================\n")
+        
+        cont = input("Continue? (y/n): ") == "y"
 
 
 if __name__ == "__main__":
